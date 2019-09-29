b79681a2d4d3bfbedde94f5bfff629284333e0eb
Shawn Huang Fernandes <sfernandes2014@gmail.com>
2019-09-29 05:30:25 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,55 @@
-# Code your solution here!
+require "pry"
+
+def run_guessing_game
+  welcomeUser
+  computerNumber = getRandNumber
+  input = getUserInput
+  checkedInput = inputCheck(input)
+  if(checkedInput == "exit")
+    exitGame
+  else
+    guessedCorrect(computerNumber, checkedInput)
+  end
+end# Code your solution here!
+
+def welcomeUser
+  "Welcome to the Guessing Game!"
+end
+
+def getRandNumber
+  rand(1..6)
+end
+
+def getUserInput
+  gets.chomp
+end
+
+def inputCheck(input) # this takes the user input, validates it, and returns it converted or re-runs this method
+  integer_input = input.to_i
+    if input == "exit"
+      transformedInput = input
+    elsif (integer_input.between?(1,6))
+      transformedInput = integer_input
+    else
+      invalid_command
+      getUserInput
+    end
+
+    transformedInput
+end
+
+def invalid_command
+  puts "Not a valid entry! Try again!"
+end
+
+def exitGame
+  puts "Goodbye!"
+end
+
+def guessedCorrect(numGenerated, numGuessed)
+  if (numGenerated == numGuessed)
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{numGenerated}."
+  end
+end
